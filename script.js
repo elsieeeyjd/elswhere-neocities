f58f6ee1a37e94599d5f70560f38bd0d89b73e6e
elsie <65228364+elsieeeyjd@users.noreply.github.com>
2025-06-09 15:02:43 +0800

    im giving up

--- script.js
+++ script.js
@@ -159,24 +159,6 @@ document.addEventListener("DOMContentLoaded", function() {
     //     document.getElementById("hit-counter").textContent = "either my code or neocities is fucked! check back later :>";
     // });
 
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         var site_data = JSON.parse(this.responseText);
-    //         var num_arr = site_data.info.views.toString().split("");
-    //         var num_str = "";
-    //         for (i = 0; i < num_arr.length; i++) {
-    //             num_str += num_arr[i];
-    //             if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
-    //             var date_str = site_data.info.last_updated;
-    //             var date_obj = new Date(site_data.info.last_updated);
-    //         }
-    //         document.getElementById("hitcount").innerHTML = num_str;
-    //     }
-    // };
-    // xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=elswhere", true);
-    // xhttp.send();
-
 });
 
  

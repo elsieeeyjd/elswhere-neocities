9bf1b15ef91c1b05947f8e692aeb04dfc357cbb0
elsie <65228364+elsieeeyjd@users.noreply.github.com>
2026-04-14 16:29:06 +0100

    more fixing

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
 
   // This will copy this folder to the output without modifying it at all
   eleventyConfig.addPassthroughCopy({ "assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "js":"js" });
 
   // This defines which files will be copied
   eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
